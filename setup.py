12b3cafc154e0209e03920162fa1b57c0338bb04
vijaye-statsig <74588208+vijaye-statsig@users.noreply.github.com>
2021-09-27 16:18:49 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setuptools.setup(
     url="https://github.com/statsig-io/ip3country-py",
     author="Statsig",
     author_email="tore@statsig.com",
-    description="A zero-dependency local, fast, tiny ip-address to country lookup",
+    description="A zero-dependency, local, fast, tiny ip-address to country lookup",
     keywords=["ip", "country", "lookup", "statsig"],
     packages=setuptools.find_packages(),
     install_requires=[],
     license="ISC",
-    python_requires=">=3.0",
+    python_requires=">=2.0",
 )
