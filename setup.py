2f66d7538fd1e3984b04d914748191d7c6541f89
vijaye-statsig <74588208+vijaye-statsig@users.noreply.github.com>
2021-09-27 13:50:00 -0700

    Fix stuff

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name="ip3country",
     version="0.1.0",
     url="https://github.com/statsig-io/ip3country-py",
@@ -11,7 +8,7 @@ setup(
     author_email="tore@statsig.com",
     description="A zero-dependency local, fast, tiny ip-address to country lookup",
     keywords=["ip", "country", "lookup", "statsig"],
-    packages=["ip3country"],
+    packages=setuptools.find_packages(),
     install_requires=[],
     license="ISC",
     python_requires=">=3.0",
